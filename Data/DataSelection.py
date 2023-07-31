7fe29fd6e724caf85c0f0f0b6833fea1e30c654e
schmitfe <felix.schmitt@uni-koeln.de>
2023-07-31 12:38:20 +0200

    Add file with the selected Data

---
+++ Data/DataSelection.py
@@ -0,0 +1,13 @@
+# We select the data for each figure and save it in a list of strings
+# The data is saved in the Data folder
+# The variables can be used in the FigureX.py files to load the data
+
+Figure2 = ['CA55.mat', 'CA57.mat', 'CA58MLR.mat', 'CA59.mat', 'CA61a.mat', 'CA61b.mat', 'CA63.mat', 'CA64.mat',
+         'CA65a.mat', 'CA65b.mat', 'CA67MLR.mat', 'CA68MLR.mat', 'CA69.mat', 'CA70a.mat', 'CA70b.mat', 'CA71.mat',
+         'CA73.mat', 'CA75o.mat', 'CA76a.mat', 'CA76b.mat', 'CA77.mat', 'CA79b.mat', 'CA80.mat', 'CA81.mat', 'CA82.mat']
+Figure3 = ['CA55.mat', 'CA57.mat', 'CA59.mat', 'CA61a.mat', 'CA61b.mat', 'CA63.mat', 'CA64.mat', 'CA65a.mat',
+           'CA65b.mat', 'CA68o.mat', 'CA69.mat', 'CA70a.mat', 'CA70b.mat', 'CA71.mat', 'CA73.mat', 'CA75o.mat',
+           'CA76a.mat', 'CA76b.mat', 'CA77.mat', 'CA79a.mat', 'CA79b.mat', 'CA80.mat', 'CA81.mat', 'CA82.mat']
+DataCorr = ['CA55.mat', 'CA57.mat', 'CA59.mat', 'CA61a.mat', 'CA61b.mat', 'CA63.mat', 'CA64.mat', 'CA65a.mat',
+            'CA65b.mat', 'CA69.mat', 'CA70a.mat', 'CA70b.mat', 'CA71.mat', 'CA73.mat', 'CA75o.mat', 'CA76a.mat',
+            'CA76b.mat', 'CA77.mat', 'CA79b.mat', 'CA80.mat', 'CA81.mat', 'CA82.mat']
